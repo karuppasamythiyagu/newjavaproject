ffb36fbb96079b0b039f98394941d3dbbb9cf70d
ajsamy2156 <rjanani671@gmail.com>
2021-11-11 10:59:30 +0530

    some project added

---
+++ src/org/student/StudentDetails.java
@@ -0,0 +1,17 @@
+package org.student;
+
+public class StudentDetails {
+	
+	private void StudentName() {
+		System.out.println("Student NAme:Karan");
+
+	}
+	private void StudentID() {
+		System.out.println("Student ID ME123");
+
+	}
+	private void StudentAddress() {
+		System.out.println("Student Address:chennai");
+	}
+
+}
